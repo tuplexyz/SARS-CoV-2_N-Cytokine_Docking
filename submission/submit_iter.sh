07635fb5cb09112c39ede970e31babaa74257622
Colby Ford <colbytylerford@gmail.com>
2023-05-17 16:30:32 -0400

    Move submit_dist and submit_iter scripts to "submission" folder

---
+++ submission/submit_iter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=cford_haddock_iter_$1
+#SBATCH --partition=Orion
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=36
+#SBATCH --constraint=skylake
+#SBATCH --mem=300gb
+#SBATCH --time=5:00:00
+
+# singularity exec haddock.sif python3 inference.py \
+#     --complex_path $2 \
+#     --complex_name $1 \
+#     --out_dir results/user_predictions_small \
+#     --inference_steps 20 \
+#     --samples_per_complex 40 \
+#     --batch_size 10 \
+#     --actual_steps 18 \
+#     --no_final_step_noise
+
+# singularity run $SINGULARITY_CONTAINER_HOME/haddock.sif
+
+cd $2
+./run-example.csh
