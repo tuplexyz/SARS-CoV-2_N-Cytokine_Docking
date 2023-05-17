1b04ee8a62c66ad4fbcbfdfe2ab53c893777a9f6
Colby Ford <colbytylerford@gmail.com>
2023-05-17 16:03:25 -0400

    Update submit_dist.sh

--- submit_dist.sh
+++ submit_dist.sh
@@ -7,20 +7,16 @@
 #SBATCH --mem=300gb
 #SBATCH --time=5:00:00
 
-export SINGULARITY_CONTAINER_HOME=/users/$USER/HADDOCK
-export SINGULARITY_TMPDIR=/scratch/$USER/tmp
+export SINGULARITY_CONTAINER_HOME=/home/gridsan/$USER/seqer_shared/N_cytokine_docking
+# export SINGULARITY_TMPDIR=/scratch/$USER/tmp
 
 export HOME=esm/model_weights
 export PYTHONPATH=$PYTHONPATH:$SINGULARITY_CONTAINER_HOME/esm
 
 export input_csv=data/HADDOCK_Results.csv
 
-module load singularity
+# module load singularity
 singularity run $SINGULARITY_CONTAINER_HOME/haddock.sif
-
-singularity exec dd.sif python3 datasets/esm_embedding_preparation.py \
-    --protein_ligand_csv $protein_ligand_csv \
-    --out_file data/prepared_for_esm.fasta
 
 while IFS="," read -r complex_id experiment_path
 do
