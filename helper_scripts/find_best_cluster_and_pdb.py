48944ae5e3dc67a4f4a91b0a87168f244a957e54
Colby Ford <colbytylerford@gmail.com>
2023-06-14 13:51:59 -0400

    Update find_best_cluster_and_pdb.py

--- helper_scripts/find_best_cluster_and_pdb.py
+++ helper_scripts/find_best_cluster_and_pdb.py
@@ -4,17 +4,22 @@ import glob, os
 directory_prefix = 'run1/structures/it1/water/'
 
 ## Get best cluster based on lowest van der Waals energy
-vdw_clusters = pd.read_csv(f'{directory_prefix}cluster_ener.txt', delimiter=r"\s+").sort_values(by=['Evdw'], ascending = True)
+# vdw_clusters = pd.read_csv(f'{directory_prefix}cluster_ener.txt', delimiter=r"\s+").sort_values(by=['Evdw'], ascending = True)
+vdw_clusters = pd.read_csv(directory_prefix+'cluster_ener.txt', delimiter=r"\s+").sort(['Evdw'], ascending = True)
 best_cluster = vdw_clusters.iat[0, 0]
 
 ## Get best PDB file from best cluster
-cluster_pdbs = pd.read_csv(f'{directory_prefix}{best_cluster}_ener', delimiter=r"\s+").sort_values(by=['Evdw'], ascending = True)
+# cluster_pdbs = pd.read_csv(f'{directory_prefix}{best_cluster}_ener', delimiter=r"\s+").sort_values(by=['Evdw'], ascending = True)
+cluster_pdbs = pd.read_csv(directory_prefix+best_cluster+'_ener', delimiter=r"\s+").sort(['Evdw'], ascending = True)
 best_pdb = cluster_pdbs.iat[0, 0]
 
 ## Make list of files to keep
-all_cluster_files = glob.glob(f'{directory_prefix}cluster_*')
-all_clusters_files = glob.glob(f'{directory_prefix}clusters*')
-best_cluster_files = glob.glob(f'{directory_prefix}{best_cluster}_*')
+# all_cluster_files = glob.glob(f'{directory_prefix}cluster_*')
+# all_clusters_files = glob.glob(f'{directory_prefix}clusters*')
+# best_cluster_files = glob.glob(f'{directory_prefix}{best_cluster}_*')
+all_cluster_files = glob.glob(directory_prefix+'cluster_*')
+all_clusters_files = glob.glob(directory_prefix+'clusters*')
+best_cluster_files = glob.glob(directory_prefix+best_cluster+'_*')
 
 files_to_keep = [
     'clusters.stat',
@@ -28,13 +33,16 @@ for root, dirs, files in os.walk('run1'):
     for file in files:
         file_path = os.path.join(root, file)
         if file_path in files_to_keep:
-            print(f'+ Keeping: {file_path}')
+            # print(f'+ Keeping: {file_path}')
+            print('+ Keeping: ' + file_path)
         else:
-            print(f'- Removing: {file_path}')
+            # print(f'- Removing: {file_path}')
+            print('- Removing: ' + file_path)
             os.remove(file_path)
 
 ## Clean up empty folders
 for folder, _, _ in list(os.walk('run1'))[::-1]:
     if len(os.listdir(folder)) == 0:
-        print(f'- Removing Folder: {folder}')
+        # print(f'- Removing Folder: {folder}')
+        print('- Removing Folder: ' + folder)
         os.rmdir(folder)
