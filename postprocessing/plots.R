4a99093a6ac1287605ce40a5522e401e6cb0037a
Colby Ford <colbytylerford@gmail.com>
2023-08-09 15:51:55 -0400

    Update plots.R

--- postprocessing/plots.R
+++ postprocessing/plots.R
@@ -1,12 +1,14 @@
 library(readr)
 library(dplyr)
+library(tidyr)
+library(stringr)
 library(ggplot2)
 library(ggpubr)
 library(forcats)
+library(ape)
+library(seqinr)
 
-experiment_results <- read_csv("experiment_results.csv")
-
-experiment_results <- experiment_results %>% 
+experiment_results <- read_csv("experiment_results.csv") %>% 
   mutate(
     cytokine_class = case_when(
       startsWith(cytokine_protein, "CCL") ~ "β-Chemokine",
@@ -20,24 +22,40 @@ experiment_results <- experiment_results %>%
     )
   )
 
+
+## Get Genetic Distances
+N_alignment <- read.alignment(file="../inputs/N-Proteins_aligned.fasta", format="fasta")
+N_dist <- dist.alignment(N_alignment) %>% 
+  as.matrix() %>% 
+  as.data.frame() %>% 
+  setNames(paste0('dist_from_', names(.))) %>% 
+  tibble::rownames_to_column(var = "n_protein") %>% 
+  mutate(n_protein = n_protein %>% str_replace("_A", ""))# %>% 
+  # select(n_protein, `dist_from_OC43-N_A`)
+  
+
+experiment_results <- experiment_results %>%
+  left_join(N_dist, by="n_protein")
+
+
 variant_order = c(
+  "OC43-N",
+  "MERS-CoV-N",
   "RaTG13-N",
   "BANAL-20-52-N",
-  "OC43-N",
   "SARS-CoV-N",
-  "MERS-CoV-N",
-  "SARS-CoV-2-WA1-N",
-  "SARS-CoV-2-B.1.351-N",
-  "SARS-CoV-2-B.1.617.2-DeltaA-N",
-  "SARS-CoV-2-B.1.1-N",
-  "SARS-CoV-2-B.1.1.7-N",
-  "SARS-CoV-2-B.1.1.529-N",
+  "SARS-CoV-2-WA1-N", # Wuhan
+  "SARS-CoV-2-B.1.1-N", #Pre-Alpha from Europe
+  "SARS-CoV-2-B.1.1.7-N",# Alpha
+  "SARS-CoV-2-B.1.351-N", # Beta
+  "SARS-CoV-2-P.1-N", # Gamma
+  "SARS-CoV-2-B.1.617.2-DeltaA-N", # Delta
+  "SARS-CoV-2-B.1.1.529-N", # Omicron
   "SARS-CoV-2-BA.1.1-N",
   "SARS-CoV-2-BA.2-N",  
   "SARS-CoV-2-BA.4-N",
-  "SARS-CoV-2-BQ.1-N",
-  "SARS-CoV-2-P.1-N",
-  "SARS-CoV-2-XBB-N"
+  "SARS-CoV-2-BQ.1-N", # Nigerian Lineage
+  "SARS-CoV-2-XBB-N" # Kraken
 )
 
 ## Box Plots
@@ -65,26 +83,70 @@ ggplot(experiment_results,
 
 
 ## Line Chart
-ggplot(experiment_results,
-       aes(x = factor(n_protein, variant_order),
+ggplot(experiment_results %>% filter(!n_protein == "OC43-N" ),
+       aes(
+         # x = factor(n_protein, variant_order),
+         x = `dist_from_OC43-N_A`,
            y = prodigy_deltaG_kcalpermol,
            group = cytokine_protein,
            color = cytokine_class
            )
        ) + 
-  geom_line() +
+  # geom_line() +
+  geom_point() +
   geom_smooth(method = lm,
               se = FALSE,
               col='grey',
               linewidth=0.7) +
   stat_cor(method = "spearman",
            aes(color = "black"),
-           label.x = 1,
-           label.y = max(experiment_results$prodigy_deltaG_kcalpermol) * 1.2) +
+           label.x = mean(experiment_results$`dist_from_OC43-N_A`),
+           # label.x = 1,
+           label.y = max(experiment_results$prodigy_deltaG_kcalpermol) * 1.2
+           ) +
   facet_wrap(~ cytokine_protein) +
   labs(y='Gibbs Energy',
-       x='Variant',
+       # x='Variant',
+       x='Distance from OC43-N',
        color = "Cytokine Class") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
         legend.position = "none")
+
+
+
+## Correlations
+
+experiment_results_pvt <- experiment_results %>% 
+  select(n_protein, cytokine_protein, prodigy_deltaG_kcalpermol, starts_with("dist_")) %>% 
+  pivot_longer(!c(n_protein, cytokine_protein, prodigy_deltaG_kcalpermol), names_to = "distance_from", values_to = "distance")
+
+correlations <- experiment_results_pvt %>% 
+  group_by(cytokine_protein, distance_from) %>% 
+  summarize(
+    correlation = cor.test(
+      x = prodigy_deltaG_kcalpermol,
+      # y = `dist_from_OC43-N_A`,
+      y = distance,
+      method = "spearman"
+      )[['estimate']],
+    p_value = cor.test(
+      x = prodigy_deltaG_kcalpermol,
+      # y = `dist_from_OC43-N_A`,
+      y = distance,
+      method = "spearman"
+    )[['p.value']],
+    ) %>% 
+  filter(p_value < 0.05)
+
+
+ggplot(correlations,
+       aes(x = distance_from,
+           y = cytokine_protein,
+           fill = correlation)) + 
+  geom_tile() +
+  labs(y='Cytokine Protein',
+       x='Distance from ...',
+       color = "Spearman Correlation") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
