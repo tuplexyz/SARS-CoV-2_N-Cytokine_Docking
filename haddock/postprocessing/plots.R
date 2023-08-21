9dbcfdcfd02351acf1fd22a60f55f2f0af48d7b6
Colby Ford <colbytylerford@gmail.com>
2023-08-21 14:26:33 -0400

    Update plots.R

--- haddock/postprocessing/plots.R
+++ haddock/postprocessing/plots.R
@@ -15,11 +15,15 @@ experiment_results <- read_csv("experiment_results.csv") %>%
       startsWith(cytokine_protein, "CX") ~ "α-Chemokine",
       startsWith(cytokine_protein, "IFN") ~ "Interferon",
       startsWith(cytokine_protein, "IL") ~ "Interleukin",
-      startsWith(cytokine_protein, "INF") ~ "Interleukin",
+      startsWith(cytokine_protein, "INF") ~ "Interferon",
       startsWith(cytokine_protein, "TNF") ~ "Tumor Necrosis Factor",
       startsWith(cytokine_protein, "XCL") ~ "γ-Chemokine",
       .default = as.character(cytokine_protein)
     )
+  ) %>% 
+  filter(
+    !startsWith(cytokine_protein, "INF"),
+    !startsWith(cytokine_protein, "IL-6R")
   )
 
 
@@ -30,12 +34,26 @@ N_dist <- dist.alignment(N_alignment) %>%
   as.data.frame() %>% 
   setNames(paste0('dist_from_', names(.))) %>% 
   tibble::rownames_to_column(var = "n_protein") %>% 
-  mutate(n_protein = n_protein %>% str_replace("_A", ""))# %>% 
-  # select(n_protein, `dist_from_OC43-N_A`)
+  mutate(n_protein = n_protein %>% str_replace("_A", "")) %>% 
+  select(n_protein, `dist_from_SARS-CoV-2-WA1-N_A`)
   
 
-experiment_results <- experiment_results %>%
-  left_join(N_dist, by="n_protein")
+experiment_results_filtered <- experiment_results %>%
+  left_join(N_dist, by="n_protein") %>% 
+  filter(cytokine_protein %in% c(
+    "CCL5",
+    "CCL11",
+    "CCL21",
+    "CCL26",
+    "CCL28",
+    "CXCL4",
+    "CXCL9",
+    "CXCL10",
+    "CXCL11",
+    "CXCL12beta",
+    "CXCL14"),
+    # !n_protein %in% c("OC43-N", "MERS-CoV-N", "SARS-CoV-N")
+  )
 
 
 variant_order = c(
@@ -59,7 +77,7 @@ variant_order = c(
 )
 
 ## Box Plots
-ggplot(experiment_results,
+ggplot(experiment_results_filtered,
        aes(x = factor(n_protein, variant_order),
            y = prodigy_deltaG_kcalpermol
            # color = cytokine_protein
@@ -68,8 +86,8 @@ ggplot(experiment_results,
   geom_boxplot() +
   # geom_jitter(shape=16,
   #             position=position_jitter(0.2)) +
-  geom_point(position=position_jitterdodge(jitter.width=0, dodge.width = 0.3, seed = 1234),
-             aes(color=factor(cytokine_protein)), show.legend = F) +
+  # geom_point(position=position_jitterdodge(jitter.width=0, dodge.width = 0.3, seed = 1337),
+  #            aes(color=factor(cytokine_protein)), show.legend = F) +
   facet_wrap(~ cytokine_class,
              nrow = 2,
              ncol = 3) +
@@ -82,11 +100,11 @@ ggplot(experiment_results,
 
 
 
-## Line Chart
-ggplot(experiment_results %>% filter(!n_protein == "OC43-N" ),
+## Line Chart (PRODIGY Delta G)
+dist_by_gibbs_line <- ggplot(experiment_results_filtered,
        aes(
          # x = factor(n_protein, variant_order),
-         x = `dist_from_OC43-N_A`,
+         x = `dist_from_SARS-CoV-2-WA1-N_A`,
            y = prodigy_deltaG_kcalpermol,
            group = cytokine_protein,
            color = cytokine_class
@@ -100,20 +118,51 @@ ggplot(experiment_results %>% filter(!n_protein == "OC43-N" ),
               linewidth=0.7) +
   stat_cor(method = "spearman",
            aes(color = "black"),
-           label.x = mean(experiment_results$`dist_from_OC43-N_A`),
+           label.x = min(experiment_results_filtered$`dist_from_SARS-CoV-2-WA1-N_A`),
            # label.x = 1,
-           label.y = max(experiment_results$prodigy_deltaG_kcalpermol) * 1.2
+           label.y = max(experiment_results_filtered$prodigy_deltaG_kcalpermol) * 1.2
            ) +
   facet_wrap(~ cytokine_protein) +
   labs(y='Gibbs Energy',
        # x='Variant',
-       x='Distance from OC43-N',
+       x='Distance from SARS-CoV-2-WA1-N',
        color = "Cytokine Class") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
         legend.position = "none")
 
 
+## Line Chart (VDW Energy)
+dist_by_vdw_line <- ggplot(experiment_results_filtered,
+       aes(
+         x = `dist_from_SARS-CoV-2-WA1-N_A`,
+         y = Evdw,
+         group = cytokine_protein,
+         color = cytokine_class
+       )
+) + 
+  geom_point() +
+  geom_smooth(method = lm,
+              se = FALSE,
+              col='grey',
+              linewidth=0.7) +
+  stat_cor(method = "spearman",
+           aes(color = "black"),
+           label.x = min(experiment_results_filtered$`dist_from_SARS-CoV-2-WA1-N_A`),
+           label.y = max(experiment_results_filtered$Evdw) * 1.2
+  ) +
+  facet_wrap(~ cytokine_protein) +
+  labs(y='van der Waals Energy',
+       x='Distance from SARS-CoV-2-WA1-N',
+       color = "Cytokine Class") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        legend.position = "none")
+
+
+ggarrange(dist_by_gibbs_line, dist_by_vdw_line, 
+          labels = c("A", "B"),
+          ncol = 1, nrow = 2)
 
 ## Correlations
 
